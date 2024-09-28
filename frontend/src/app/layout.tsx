7e2d972b52ad6cd013e6c6b364c4650c3a2de1b9
Levi Francis <levifrancis15@gmail.com>
2024-09-28 23:24:33 +0100

    set up viem and connect button bare bones

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -17,9 +17,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>
-        <Providers>{children}</Providers>
-        <Toaster />
+    <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
       </body>
     </html>
   );
