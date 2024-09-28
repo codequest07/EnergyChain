c98b2abfd695a824ff9bc96e1cc259dc04b56038
Levi Francis <levifrancis15@gmail.com>
2024-09-28 23:24:33 +0100

    set up viem and connect button bare bones

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -1,8 +1,14 @@
+import { ConnectButton } from '@rainbow-me/rainbowkit';
+
 export default function NavBarr() {
   return (
-    <header className="bg-black fixed top-0 w-full z-10 text-white p-4">
+    <header className="bg-black flex justify-between fixed top-0 w-full z-10 text-white p-4">
       <div className="container mx-auto">
         <h1 className="text-2xl font-bold">DenLink</h1>
+      </div>
+
+      <div>
+        <ConnectButton />
       </div>
     </header>
   );
