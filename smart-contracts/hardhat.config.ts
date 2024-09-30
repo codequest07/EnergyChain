83b6d6d8f3dd59ed3c84fa4d84d7e3e068ef2ec1
Michael Nwachukwu <dahformulah@gmail.com>
2024-09-30 01:57:16 +0100

    Setup:Setup on hardhat config. README:Update on readme

--- smart-contracts/hardhat.config.ts
+++ smart-contracts/hardhat.config.ts
@@ -2,12 +2,14 @@ import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import * as dotenv from "dotenv";
 dotenv.config();
+import * as dotenv from "dotenv";
+dotenv.config();
 
 const config: HardhatUserConfig = {
   solidity: "0.8.24",
   networks: {
     // for testnet
-    "scrollSepolia": {
+    "scroll-sepolia": {
       url: process.env.ALCHEMY_SCROLL_RPC_URL!,
       accounts: [process.env.ACCOUNT_PRIVATE_KEY!],
       gasPrice: 1000000000,
