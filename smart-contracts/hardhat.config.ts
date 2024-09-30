cd0c5bc2520f3130f9d4ac1657342756c4243ef1
Michael Nwachukwu <dahformulah@gmail.com>
2024-09-30 02:24:11 +0100

    Deployment modules and smart contracts

--- smart-contracts/hardhat.config.ts
+++ smart-contracts/hardhat.config.ts
@@ -7,7 +7,7 @@ const config: HardhatUserConfig = {
   solidity: "0.8.24",
   networks: {
     // for testnet
-    "scroll-sepolia": {
+    "scrollSepolia": {
       url: process.env.ALCHEMY_SCROLL_RPC_URL!,
       accounts: [process.env.ACCOUNT_PRIVATE_KEY!],
       gasPrice: 1000000000,
