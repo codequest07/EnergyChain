df633eadda5899d84a98902d4259b6ac32257b71
AMD <michojekunle1@gmail.com>
2024-10-08 05:37:35 +0100

    auth create account page scaffold

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,13 +1,17 @@
 import type { Metadata } from "next";
+import { DM_Sans, Grenze } from 'next/font/google';
 import "./globals.css";
 import "@rainbow-me/rainbowkit/styles.css";
+import '@coinbase/onchainkit/styles.css';
+
+const dmSans = DM_Sans({weight: ["300", "400", "500", "600", "700", "900"], subsets: ['latin']})
 
 import { Providers } from "./providers";
 import { Toaster } from "@/components/ui/toaster";
 
 export const metadata: Metadata = {
-  title: "energy-chain",
-  description: "energy-chain",
+  title: "Energy-chain",
+  description: "Energy-chain",
 };
 
 export default function RootLayout({
@@ -17,7 +21,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>
+      <body className={`${dmSans.className} antialiased`}>
         <Providers>{children}</Providers>
         <Toaster />
       </body>
