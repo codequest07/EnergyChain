87d7496487326ea4874fdd8eead93e874987a605
Levi Francis <levifrancis15@gmail.com>
2024-10-09 01:48:03 +0100

    add energy chart and assets table

--- frontend/src/components/DashMain.tsx
+++ frontend/src/components/DashMain.tsx
@@ -3,11 +3,15 @@ import { Statistics } from "@/utils/data";
 import ProsumerScoreCard from "./cards/ProsumerScoreCard";
 import StatisticCard from "./cards/StatisticCard";
 import EnergyMetrics from "./Energy-metrics";
+import { TotalEnergyChart } from "./TotalEnergyChart";
+import { MarketplaceActivity } from "./MarketplaceActivity";
+import { Assets } from "./Assets";
 const DashMain = () => {
   return (
     <div className="flex flex-col min-h-screen">
-      <div className=" p-4 md:p-8 pt-6">
+      <div className="flex gap-4 p-4 md:p-8 pt-6">
         <ProsumerScoreCard />
+        <TotalEnergyChart />
       </div>
       <div className="grid sm:grid-cols-4 grid-cols-1 gap-3 p-4">
         {Statistics.map((data, index) => (
@@ -26,6 +30,13 @@ const DashMain = () => {
       <div className="p-4">
         <EnergyMetrics />
       </div>
+      {/* max-w-7xl mx-auto */}
+      <div className=" p-4">
+         <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+           <MarketplaceActivity />
+           <Assets />
+         </div>
+       </div>
     </div>
   );
 };
